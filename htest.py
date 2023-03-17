12f0df2486b67a42ba06b14aacdd04cdcadda473
Abdullah_Alamodi <abdullah-alamodi@outlook.com>
2023-03-17 20:47:06 +0300

    update

--- htest.py
+++ htest.py
@@ -1,5 +1,5 @@
 from haraj_scraper import get_haraj_links, get_haraj_details
 
-links = get_haraj_links(nu_of_pages=1)
+links = get_haraj_links(nu_of_pages=2)
 print(links)
 get_haraj_details(links, save=True, save_format="csv")
